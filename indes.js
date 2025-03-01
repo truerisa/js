de13e91a05f3119817fc5c0e50ae458b3c4dc0ce
Richard Metzner <risapowered@gmail.com>
2025-03-01 18:53:40 +0100

    JS statements

--- indes.js
+++ indes.js
@@ -4,3 +4,8 @@ let x, y, z;    // Statement 1
 x = 5;          // Statement 2
 y = 6;          // Statement 3
 z = x + y;      // Statement 4
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
